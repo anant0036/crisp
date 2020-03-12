63d79ce27872d4622a70ba2b36996e4aa2ada8c2
Anant Bhushan <anant0036@gmail.com>
2020-03-12 20:59:27 +0530

    all_post

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,4 +1,40 @@
-<form action="" method="" enctype="multipart/form-data">
+<?php
+
+    if(isset($_POST['create_post']))
+    {
+
+        $post_title         = $_POST['title'];
+        $post_author        = $_POST['post_author'];
+        $post_category_id   = $_POST['post_category_id'];
+        $post_status        = $_POST['post_status'];
+
+        $post_image         = $_FILES['image']['name'];
+        $post_image_temp    = $_FILES['image']['tmp_name'];
+
+        $post_tags          = $_POST['post_tags'];
+        $post_content       = $_POST['post_content'];
+        $post_date          = date('d-m-y');
+        $post_comment_count = 4;
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        // $query = "INSERT INTO posts(post_category_id,post_title,post_author,post_date,post_image,post_content,post_tags,post_comment_count,post_status)";
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date,post_image,post_content,post_tags,post_status) ";
+        
+        // $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags},'{$post_comment_count}','{$post_status}')";
+        $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') "; 
+        $create_post_query = mysqli_query($connection, $query)or die("querybhu error");
+
+        if(false===$query)
+        echo "<br/>".mysqli_error($connection)."<br/><br/><br/>";
+        else
+        echo "<br/>done ";
+    }
+
+?>
+
+
+<form action="" method="post" enctype="multipart/form-data">
 
 <div class="form-group">
     <label for="title">Post Title</label>
