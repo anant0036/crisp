6b59f478d346d1d101cda6a272de9dea802ce6f0
Anant Bhushan <anant0036@gmail.com>
2020-03-25 01:48:20 +0530

    LOGIN & PROFILE :)

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,4 +1,16 @@
 <?php ob_start(); ?>
+<?php session_start(); ?>
+
+<?php
+
+if(!isset($_SESSION['user_role']))
+{
+    header("Location: ../index.php");
+}
+
+?>
+
+
 
 <?php include "../includes/db.php" ?>
 <?php include "function.php" ?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -31,3 +31,26 @@
         </div>
         <!-- /.row -->
 </div>
+
+
+<!-- login -->
+<div class="well">
+
+<h4>login</h4>
+<form action="includes/login.php" method="post">
+
+<div class="form-group">
+    <input name="username" type="text" class="form-control" placeholder="Enter Username">
+</div>
+<div class="form-group">
+    <input name="password" type="password" class="form-control" placeholder="Enter Password">
+</div>
+
+<span class="input-group-btn">
+    <button class="btn btn-success" name="login" type="submit">Submit</button>
+</span>
+
+
+</form>
+
+</div>
